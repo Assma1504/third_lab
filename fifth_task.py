e5ec48b7ab3a705bf558198164b803e62797a37f
Assma1504 <assmabaziz15@gamil.com>
2026-03-18 17:43:38 +0300

    fifth task done

---
+++ fifth_task.py
@@ -0,0 +1,9 @@
+firstOp = 5 + 2 * 3 - 4 / 2
+secondOp = 	(3 + 5) * (2 + 4) / 2 
+thirdOp = -3 + 6 / 2 * 4 
+fourthOp = 5+4*5**2+7 
+
+print(f"The result off the first operation is: {firstOp}")
+print(f"The result off the second operation is: {secondOp}")
+print(f"The result off the third operation is: {thirdOp}")
+print(f"The result off the fourth operation is: {fourthOp}")
