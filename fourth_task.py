b757492653a924994d0488f7c5923c2fca2f891b
Assma1504 <assmabaziz15@gamil.com>
2026-03-18 17:37:52 +0300

    fourth task done

---
+++ fourth_task.py
@@ -0,0 +1,5 @@
+firstNum = int(input("Please enter the first number: "))
+secondNum = int(input("Please enter the second number: "))
+thirdNum = int(input("Please enter the third number: "))
+arithmeticMean = (firstNum + secondNum + thirdNum) / 3
+print(f"The arithmetic mean of the entered numbers is : {arithmeticMean}")
