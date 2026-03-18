42ae393d7bb21ca09e28d792b7ca2f14fd55d5c3
Assma1504 <assmabaziz15@gamil.com>
2026-03-18 17:33:53 +0300

    third task done

---
+++ third_task.py
@@ -0,0 +1,3 @@
+temperatureCelsius = float(input("Please enter the temperature that you want to convert: "))
+temperatureFahrenheit = temperatureCelsius * 9/5 + 32
+print(f"The temperature after convert is: {temperatureFahrenheit} F")
